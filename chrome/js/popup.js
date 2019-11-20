506a68b2a337a26a79ac9b686a361179697d7cae
ccsplit <cshay237@gmail.com>
2019-11-20 13:35:59 -0600

    Add a button to copy out the different URLs.
    It will separate them into the Vulnerability URLs and also
    the component URLs which is useful. Need to possible add in what
    components were affected, such as Angular, jQuery, etc.

--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function() {
 	}, false);
 	queryForResults();
 	setInterval(queryForResults, 5000);
-
+  document.querySelector("#copy_urls").addEventListener("click", copyUrls);
 }, false);
 
 function queryForResults() {
@@ -19,6 +19,65 @@ function queryForResults() {
 	    console.log(response);
 	  });
 	});	
+}
+
+function copyToClipboard(str) {
+    /* ——— Derived from: https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+           improved to add iOS device compatibility——— */
+    const el = document.createElement('textarea'); // Create a <textarea> element
+
+    let storeContentEditable = el.contentEditable;
+    let storeReadOnly = el.readOnly;
+
+    el.value = str; // Set its value to the string that you want copied
+    el.contentEditable = true;
+    el.readOnly = false;
+    el.setAttribute('readonly', false); // Make it readonly false for iOS compatability
+    el.setAttribute('contenteditable', true); // Make it editable for iOS
+    el.style.position = 'absolute';
+    el.style.left = '-9999px'; // Move outside the screen to make it invisible
+    document.body.appendChild(el); // Append the <textarea> element to the HTML document
+    const selected =
+        document.getSelection().rangeCount > 0 // Check if there is any content selected previously
+            ? document.getSelection().getRangeAt(0) // Store selection if found
+            : false; // Mark as false to know no selection existed before
+    el.select(); // Select the <textarea> content
+    el.setSelectionRange(0, 999999);
+    document.execCommand('copy'); // Copy - only works as a result of a user action (e.g. click events)
+    document.body.removeChild(el); // Remove the <textarea> element
+    if (selected) {
+        // If a selection existed before copying
+        document.getSelection().removeAllRanges(); // Unselect everything on the HTML document
+        document.getSelection().addRange(selected); // Restore the original selection
+    }
+
+    el.contentEditable = storeContentEditable;
+    el.readOnly = storeReadOnly;
+}
+
+function copyUrls() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {getDetected: 1}, function(response){
+      components = new Set()
+      urls = new Set()
+      response.forEach(function(vuln){
+        if (vuln.vulnerable) {
+          components.add(vuln.url);
+          vuln.results.forEach(function(res) {
+            if (res.vulnerabilities.length > 0) {
+              res.vulnerabilities.forEach(function(res_vuln){
+                res_vuln.info.forEach(function(i){
+                  urls.add(i);
+                })
+              });
+            }
+          });
+        }
+      })
+      copyToClipboard(`Vulnerability URLs:\n${Array.from(urls).sort().join('\n')}\n\nComponents:\n${Array.from(components).sort().join("\n")}`)
+      alert("Successfully copied.")
+    })
+  })
 }
 
 function show(results) {
